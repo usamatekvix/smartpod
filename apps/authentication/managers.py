95c198e4dcc8c034dad5a13528b34a5c5a3cd49e
Your Name <you@example.com>
2025-02-21 12:48:28 +0500

    implement authentication with djoser only JWT

---
+++ apps/authentication/managers.py
@@ -0,0 +1,58 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, first_name, last_name, email, password=None, **extra_fields):
+        """Creates and returns a regular user."""
+        if not username:
+            raise ValueError(_("Users must submit a username"))
+        if not first_name:
+            raise ValueError(_("Users must submit a first name"))
+        if not last_name:
+            raise ValueError(_("Users must submit a last name"))
+        if not email:
+            raise ValueError(_("Base User Account: An email address is required"))
+
+        email = self.normalize_email(email)
+        try:
+            validate_email(email)
+        except ValidationError:
+            raise ValueError(_("You must provide a valid email address"))
+
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+
+        user = self.model(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            **extra_fields
+        )
+        user.set_password(password)  
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, first_name, last_name, email, password=None, **extra_fields):
+        """Creates and returns a superuser."""
+        if not password:
+            raise ValueError(_("Superusers must have a password"))
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields["is_staff"] is not True:
+            raise ValueError(_("Superusers must have is_staff=True"))
+        if extra_fields["is_superuser"] is not True:
+            raise ValueError(_("Superusers must have is_superuser=True"))
+
+        return self.create_user(username, first_name, last_name, email, password, **extra_fields)
+
+
+        
+
+
+
---
+++ main/settings/development.py
@@ -0,0 +1,22 @@
+from .base import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': env('POSTGRES_ENGINE'),
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': env('POSTGRES_PORT'),
+    }
+}
+
+# EMAIL CONFIGURATION
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = "info@xyz.com"
